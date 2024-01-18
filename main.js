09d79119f0df10e224bf040de598dc68d9587a92
ankit0823 <139673494+ankit0823@users.noreply.github.com>
2024-01-18 21:28:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text1", {
-    strings: ["Frontend Devloper" , "Web Devloper"],
+    strings: ["Frontend Developer" , "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
